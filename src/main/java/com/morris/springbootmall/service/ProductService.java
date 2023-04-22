1ebb53fe223595d59363b6acdda85e9f85dbdf1c
LAPTOP-PH03R9UH\morris <morris91re@gmail.com>
2023-04-22 14:30:04 +0800

    feat: 新增SELECT List商品列表

--- src/main/java/com/morris/springbootmall/service/ProductService.java
+++ src/main/java/com/morris/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.morris.springbootmall.service;
 import com.morris.springbootmall.dto.ProductRequest;
 import com.morris.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
