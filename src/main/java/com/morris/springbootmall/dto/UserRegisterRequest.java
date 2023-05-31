8032dbe317096a09674b97823af3b3f9a4c6c532
LAPTOP-PH03R9UH\morris <morris91re@gmail.com>
2023-05-31 21:00:22 +0800

    feat:帳號功能-檢查註冊的 email、隱藏返回的密碼

--- src/main/java/com/morris/springbootmall/dto/UserRegisterRequest.java
+++ src/main/java/com/morris/springbootmall/dto/UserRegisterRequest.java
@@ -1,11 +1,13 @@
 package com.morris.springbootmall.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserRegisterRequest {
 
+    @Email
     @NotBlank
     private String email;
     @NotBlank
--- src/main/java/com/morris/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/morris/springbootmall/service/impl/UserServiceImpl.java
@@ -4,11 +4,19 @@ import com.morris.springbootmall.dao.UserDao;
 import com.morris.springbootmall.dto.UserRegisterRequest;
 import com.morris.springbootmall.model.User;
 import com.morris.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -20,6 +28,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
-       return userDao.createUser(userRegisterRequest);
+        // 檢查註冊Email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user != null) {
+            log.warn("該 email {} 已經註冊過", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // 創建帳號
+        return userDao.createUser(userRegisterRequest);
     }
 }
